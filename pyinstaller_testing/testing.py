6cab8c38df09375adaffd506060419d44eb10b8b
Dani <dani.cstech@gmail.com>
2021-08-22 09:20:41 +0800

    changed directory of git

---
+++ pyinstaller_testing/testing.py
@@ -0,0 +1,3 @@
+print("Hello! This is for a test in pyinstaller. No need for extra arguments, just clicked the file.")
+
+input("Press any key to exit")
---
+++ pyinstaller_testing/testing_filestream.py
@@ -0,0 +1,6 @@
+print("Hello! This is for a test in pyinstaller. No need for extra arguments, just clicked the file.")
+
+with open("input_file.txt") as f:
+    print(f.read())
+
+input("Press any key to exit")
